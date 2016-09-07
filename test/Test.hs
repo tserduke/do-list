1873ccdd979507088c5373fd0036550632982c4d
Taras Serduke <taras.serduke@gmail.com>
2016-09-07 11:01:14 +0300

    test

---
+++ test/Test.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+import Test.Hspec
+
+import Data.DoList
+
+
+main :: IO ()
+main = hspec $ describe "DoList" $ do
+  it "(>>)" $
+    toList (item 1 >> item 2) `shouldBe` ([1, 2] :: [Int])
