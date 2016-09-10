b09e2e3fd9ffafc080d55e693c83d120b44fe38d
Taras Serduke <taras.serduke@gmail.com>
2016-09-10 12:18:58 +0300

    0.9.1

--- publish.sh
+++ publish.sh
@@ -4,7 +4,6 @@ if ./check.sh; then
 	cabal clean
 	cabal configure
 	cabal build
-	cabal sdist --output-directory ./dist/package
-        tar -czf ./dist/package.tar.gz ./dist/package
-	cabal upload ./dist/package.tar.gz
+	cabal sdist
+	cabal upload ./dist/do-list-*.tar.gz
 fi
