55e6b8c27367c6473f647406eb48eb6f403cac55
Taras Serduke <taras.serduke@gmail.com>
2016-09-11 09:18:15 +0300

    shorter benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -8,38 +8,45 @@ import Data.DoList
 
 main :: IO ()
 main = defaultMain $ toList $ do
-  item $ bgroup "Sum" $ toList $ do
-    item $ bench "Writer" $ whnf execWriter $ do
+  benchGroup "Sum" $ do
+    whnfBench "Writer" execWriter $ do
       tell $ Sum (1 :: Int)
       tell $ Sum 2
       tell $ Sum 3
       tell $ Sum 4
       tell $ Sum 5
-    item $ bench "DoList" $ whnf (mconcat . toList) $ do
+    whnfBench "DoList" (mconcat . toList) $ do
       item $ Sum (1 :: Int)
       item $ Sum 2
       item $ Sum 3
       item $ Sum 4
       item $ Sum 5
-  item $ bgroup "List" $ toList $ do
-    item $ bench "Writer" $ whnf (last . execWriter) $ do
+  benchGroup "List" $ do
+    whnfBench "Writer" (last . execWriter) $ do
       tell [1 :: Int]
       tell [2]
       tell [3]
       tell [4]
       tell [5]
-    item $ bench "DoList" $ whnf (last . toList) $ do
+    whnfBench "DoList" (last . toList) $ do
       item (1 :: Int)
       item 2
       item 3
       item 4
       item 5
-  item $ bgroup "Combined List" $ toList $ do
-    item $ bench "Writer" $ whnf (last . execWriter) $ do
+  benchGroup "Combined List" $ do
+    whnfBench "Writer" (last . execWriter) $ do
       tell [1 :: Int, 2, 3]
       tell [4, 5, 6]
       tell [7, 8, 9]
-    item $ bench "DoList" $ whnf (last . toList) $ do
+    whnfBench "DoList" (last . toList) $ do
       fromList [1 :: Int, 2, 3]
       fromList [3, 4, 5]
       fromList [7, 9, 9]
+
+
+benchGroup :: String -> DoList Benchmark -> DoList Benchmark
+benchGroup name = item . bgroup name . toList
+
+whnfBench :: String -> (a -> b) -> a -> DoList Benchmark
+whnfBench name func = item . bench name . whnf func
