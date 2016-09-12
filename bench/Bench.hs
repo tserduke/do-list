ccf9ad3fb4ba66d05bb2cbf45540f298230b6991
Taras Serduke <taras.serduke@gmail.com>
2016-09-12 19:15:51 +0300

    text benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -6,6 +6,7 @@ import Control.Monad.Writer
 import Data.DoList
 import Data.DoMonoid
 
+import Data.Text as T (Text, unlines)
 
 main :: IO ()
 main = defaultMain $ toList $ do
@@ -16,6 +17,10 @@ main = defaultMain $ toList $ do
   benchGroup "List" $ do
     whnfBench "Writer" (last . listWriter 1 2 3 4) 5
     whnfBench "DoList" (last . listList   1 2 3 4) 5
+  benchGroup "Lines" $ do
+    whnfBench "Writer" (linesWriter "Line 1\n" "Line 2\n") "Line 3\n"
+    whnfBench "List"   (linesList   "Line 1"   "Line 2"  ) "Line 3"
+    whnfBench "Monoid" (linesMonoid "Line 1\n" "Line 2\n") "Line 3\n"
 
 
 sumWriter, sumList, sumMonoid :: Int -> Int -> Int -> Int -> Int -> Sum Int
@@ -62,6 +67,28 @@ listList x1 x2 x3 x4 x5 = toList $ do
   item x3
   item x4
   item x5
+
+
+linesWriter, linesList, linesMonoid :: Text -> Text -> Text -> Text
+
+{-# NOINLINE linesWriter #-}
+linesWriter x1 x2 x3 = x `seq` x where
+  x = execWriter $ do
+    tell x1
+    tell x2
+    tell x3
+
+{-# NOINLINE linesList #-}
+linesList x1 x2 x3 = T.unlines $ toList $ do
+  item x1
+  item x2
+  item x3
+
+{-# NOINLINE linesMonoid #-}
+linesMonoid x1 x2 x3 = runDoM $ do
+  DoM x1
+  DoM x2
+  DoM x3
 
 
 benchGroup :: String -> DoList Benchmark -> DoList Benchmark
